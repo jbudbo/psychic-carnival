aae0cfdff01705a64eccbc8ef7be8760bfe38c33
Justin Morris <Justin.Morris@bd.com>
2025-03-20 12:48:16 -0400

    fixed install issue3

--- docs/pinewood/sw.js
+++ docs/pinewood/sw.js
@@ -6,7 +6,7 @@ e.waitUntil(
     (async () => {
     const cache = await caches.open(cacheName);
     console.log("[Service Worker] Caching all: app shell and content");
-    await cache.addAll(contentToCache);
+    await cache.addAll(['/']);
     })(),
 );
 });
