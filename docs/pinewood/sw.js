2eb93ec2e0020c8ee6638632acffd69f49601988
Justin Morris <Justin.Morris@bd.com>
2025-03-20 12:00:32 -0400

    fixed install issue2

--- docs/pinewood/sw.js
+++ docs/pinewood/sw.js
@@ -1,30 +1,30 @@
-self.addEventListener("install", (e) => {
-    console.log("[Service Worker] Install");
-    e.waitUntil(
-      (async () => {
-        const cache = await caches.open(cacheName);
-        console.log("[Service Worker] Caching all: app shell and content");
-        await cache.addAll(contentToCache);
-      })(),
-    );
-  });
-  
-  
+const cacheName = "pack-43-pvt";
 
-  self.addEventListener("fetch", (e) => {
+self.addEventListener("install", (e) => {
+console.log("[Service Worker] Install");
+e.waitUntil(
+    (async () => {
+    const cache = await caches.open(cacheName);
+    console.log("[Service Worker] Caching all: app shell and content");
+    await cache.addAll(contentToCache);
+    })(),
+);
+});
+  
+self.addEventListener("fetch", (e) => {
     e.respondWith(
-      (async () => {
+        (async () => {
         const r = await caches.match(e.request);
         console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
         if (r) {
-          return r;
+            return r;
         }
         const response = await fetch(e.request);
         const cache = await caches.open(cacheName);
         console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
         cache.put(e.request, response.clone());
         return response;
-      })(),
+        })(),
     );
-  });
+});
   
