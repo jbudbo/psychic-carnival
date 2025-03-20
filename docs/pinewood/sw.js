7fa42bdcc77080f77ece8022d3f045f4a58e15fd
Justin Morris <Justin.Morris@bd.com>
2025-03-20 13:07:27 -0400

    assets

--- docs/pinewood/sw.js
+++ docs/pinewood/sw.js
@@ -4,14 +4,9 @@ self.addEventListener("install", (e) => {
 console.log("[Service Worker] Install");
 e.waitUntil(
     (async () => {
-    const cache = await caches.open(cacheName);
-    console.log("[Service Worker] Caching all: app shell and content");
-    await cache.addAll([
-        "./index.html",
-        "./script.js",
-        "./style.css",
-        "./manifest.json"
-    ]);
+        const cache = await caches.open(cacheName);
+        console.log("[Service Worker] Caching all: app shell and content");
+        await cache.addAll(['./']);
     })(),
 );
 });
